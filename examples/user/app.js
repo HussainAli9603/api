bc4b279addf67195e709cfbd23f2974ae1f21d42
HussainAli9603 <72409657+HussainAli9603@users.noreply.github.com>
2021-09-22 06:07:30 -0700

    expample

--- examples/user/app.js
+++ examples/user/app.js
@@ -4,12 +4,12 @@ const fetchData = async () => {
   try {
     const { data } = await axios.get('/api/user')
     const products = data.map((product) => {
-      const { image: { url }, name, price, } = product
+      const { image: { url }, name, email, } = product
       return `<article class="product">
                 <img src="${url}" alt="${name}"/>
                   <div class="info">
                     <h5>${name}</h5>
-                    <h5 class="price">$${price}</h5>
+                    <h5 class="price">{email}</h5>
                   </div>
               </article>`
     }).join('')
